4db2bb41d91616ccb80a7ac038a7224859b6bb2e
İHLASMERT <ihlas1mert@gmail.com>
2022-12-30 23:19:50 +0300

    added fi else statement

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ let ranFactBtn = document.getElementById("ran-fact-btn");
 let fact = document.querySelector(".fact");
 let url = "http://numbersapi.com/";
 
-let fetchFact =(num)=>{
-    let finalUrl = url + num;
-    fetch(finalUrl)
-    .then((resp)=>resp.text())
-    .then((data)=>{
-        fact.innerHTML = `<h2>${num}</h2>
+let fetchFact = (num) => {
+  let finalUrl = url + num;
+  fetch(finalUrl)
+    .then((resp) => resp.text())
+    .then((data) => {
+      fact.innerHTML = `<h2>${num}</h2>
         <p>${data}</p>`;
-        document.querySelector(".container").append
-        (fact)
-    })
-}
+      document.querySelector(".container").append(fact);
+    });
+};
 
-let getFact =()=>{
-    let num = document.getElementById("num").value
-}
+let getFact = () => {
+  let num = document.getElementById("num").value;
+  if (num) {
+    if (num >= 0 && num <= 300) {
+      fetch(num);
+    } else {
+      fact.getElementsByClassName.display = "block";
+      fact.innerHTML = `<p class="msg">Please enter a number between 0 to 300.</p>`;
+    }
+  }else{
+    fact.getElementsByClassName.display = "block";
+    fact.innerHTML = `<p class="msg">The input filed cannot be empty</p>`
+  }
+};
